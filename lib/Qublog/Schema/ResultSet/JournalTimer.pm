5ff17a754132759f0aea5d2e902d1ffe7487e352
Sterling Hanenkamp <hanenkamp@cpan.org>
2009-09-18 13:11:06 -0500

    Adding a custom resultset extension for timers

---
+++ lib/Qublog/Schema/ResultSet/JournalTimer.pm
@@ -0,0 +1,13 @@
+package Qublog::Schema::ResultSet::JournalTimer;
+use Moose;
+extends qw( DBIx::Class::ResultSet );
+
+sub search_by_running {
+    my ($self, $running) = @_;
+
+    return $self->search({ 
+        stop_time => { ($running ? '!=' : '=='), undef },
+    });
+}
+
+1;

1d6458eccbbcdbe8b99038e0f01b7cc0d419ce3a
Sterling Hanenkamp <hanenkamp@cpan.org>
2009-09-18 13:10:04 -0500

    Adding a stub for the help view

---
+++ lib/Qublog/Server/View/TD/Help.pm
@@ -0,0 +1,11 @@
+package Qublog::Server::View::TD::Journal;
+use strict;
+use warnings;
+
+use Template::Declare::Tags;
+
+template 'help/journal/new_comment_entry' => sub {
+    div { 'Not Yet Implemented' };
+};
+
+1;

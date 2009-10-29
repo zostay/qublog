d51d5ceecf1bc05b524eaa38e64f8b3eb7e4018f
Sterling Hanenkamp <hanenkamp@cpan.org>
2009-10-28 21:24:02 -0500

    Correcting the license in Qublog::Server::Model::DB

--- lib/Qublog/Server/Model/DB.pm
+++ lib/Qublog/Server/Model/DB.pm
@@ -27,12 +27,25 @@ Catalyst::Helper::Model::DBIC::Schema - 0.28
 
 =head1 AUTHOR
 
-Andrew Sterling Hanenkamp
+Andrew Sterling Hanenkamp, C<< <hanenkamp@cpan.org> >>
 
 =head1 LICENSE
 
-This library is free software, you can redistribute it and/or modify
-it under the same terms as Perl itself.
+Qublog Personal/Professional Journaling
+Copyright (C) 2009  Andrew Sterling Hanenkamp
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 =cut
 

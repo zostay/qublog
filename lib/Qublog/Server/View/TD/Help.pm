8f16edad5a5413135ed8b0b6e91749855f52a071
Sterling Hanenkamp <hanenkamp@cpan.org>
2009-10-29 09:42:24 -0500

    Adding POD to Qublog::Server::View::TD::Help

--- lib/Qublog/Server/View/TD/Help.pm
+++ lib/Qublog/Server/View/TD/Help.pm
@@ -4,7 +4,49 @@ use warnings;
 
 use Template::Declare::Tags;
 
+=head1 NAME
+
+Qublog::Server::View::TD::Help - help templates
+
+=head1 DESCRIPTION
+
+Sometimes people need some help.
+
+=head1 TEMPLATES
+
+=head2 help/journal/new_comment_entry
+
+Show help for the new comment entry.
+
+Not yet implemented as of this writing.
+
+=cut
+
 template 'help/journal/new_comment_entry' => sub {
 };
+
+=head1 AUTHOR
+
+Andrew Sterling Hanenkamp, C<< <hanenkamp@cpan.org> >>
+
+=head1 LICENSE
+
+Qublog Personal/Professional Journaling
+Copyright (C) 2009  Andrew Sterling Hanenkamp
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
 
 1;

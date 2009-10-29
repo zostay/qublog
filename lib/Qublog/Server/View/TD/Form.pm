7d228660b4e5b21880bdfa6f0a8222d39de9b339
Sterling Hanenkamp <hanenkamp@cpan.org>
2009-10-29 08:46:12 -0500

    Adding POD to Qublog::Server::View::TD::Form

--- lib/Qublog/Server/View/TD/Form.pm
+++ lib/Qublog/Server/View/TD/Form.pm
@@ -8,6 +8,35 @@ use Template::Declare::Tags;
 use Qublog::Web;
 use Qublog::Server::View::Common;
 
+=head1 NAME
+
+Qublog::Server::View::TD::Form - various form templates used with journal items
+
+=head1 DESCRIPTION
+
+Journal items may have forms attached to them by actions. This contains those
+forms.
+
+=head1 TEMPLATES
+
+=head2 form/change_start_stop
+
+This displays a form for changing the start or stop time of a timer. It expects
+the following stash arguments:
+
+=over
+
+=item journal_timer
+
+This is the L<Qublog::Schema::Result::JournalTimer> to edit.
+
+=back
+
+The request for this form should include a C<which> argument, set to "start" or
+"stop" to select which time on the timer to edit.
+
+=cut
+
 template 'form/change_start_stop' => sub {
     my ($self, $c) = @_;
     my $timer  = $c->stash->{journal_timer};
@@ -79,6 +108,20 @@ template 'form/change_start_stop' => sub {
     };
 };
 
+=head2 form/edit_entry
+
+Show a form to edit a L<Qublog::Server::VIew::TD::Form>.
+
+=over
+
+=item journal_entry
+
+The journal entry to edit.
+
+=back
+
+=cut
+
 template 'form/edit_entry' => sub {
     my ($self, $c) = @_;
     my $entry = $c->stash->{journal_entry};
@@ -146,6 +189,15 @@ template 'form/edit_entry' => sub {
     };
 };
 
+=head2 form/list_actions/project_summary
+
+This is not meant to be used directly. This displays the tasks for a given
+project.
+
+See L</form/list_actions>.
+
+=cut
+
 template 'form/list_actions/project_summary' => sub {
     my ($self, $c, $project) = @_;
 
@@ -173,6 +225,22 @@ template 'form/list_actions/project_summary' => sub {
     };
 };
 
+=head2 form/list_actions
+
+This shows the tasks for a journal entry's project.
+
+Uses the following stashed arguments:
+
+=over
+
+=item journal_entry
+
+This is the L<Qublog::Schema::Result::JournalEntry> to list actions for.
+
+=back
+
+=cut
+
 template 'form/list_actions' => sub {
     my ($self, $c) = @_;
     my $entry = $c->stash->{journal_entry};
@@ -187,6 +255,22 @@ template 'form/list_actions' => sub {
         };
     };
 };
+
+=head2 form/edit_comment
+
+Edit the timer and text of a comment.
+
+Uses these stashed arguments:
+
+=over
+
+=item comment
+
+This is the L<Qublog::Schema::Result::Comment> to edit.
+
+=back
+
+=cut
 
 template 'form/edit_comment' => sub {
     my ($self, $c) = @_;
@@ -245,5 +329,29 @@ template 'form/edit_comment' => sub {
         };
     };
 };
+
+=head1 AUTHOR
+
+Andrew Sterling Hanenkamp, C<< <hanenkamp@cpan.org> >>
+
+=head1 LICENSE
+
+Qublog Personal/Professional Journaling
+Copyright (C) 2009  Andrew Sterling Hanenkamp
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
 
 1;

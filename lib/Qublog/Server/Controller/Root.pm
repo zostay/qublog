1a839598c498e0ad1b82945ff897f7c9d815be95
Sterling Hanenkamp <hanenkamp@cpan.org>
2009-10-28 21:16:15 -0500

    Fixing and filling Qublog::Server::Controller::Root's POD

--- lib/Qublog/Server/Controller/Root.pm
+++ lib/Qublog/Server/Controller/Root.pm
@@ -16,13 +16,15 @@ Qublog::Server::Controller::Root - Root Controller for Qublog::Server
 
 =head1 DESCRIPTION
 
-[enter your description here]
+Handles the root namespace, which doesn't actually exist. This controller exists
+to forward on requests to either L<Qublog::Server::Controller::Journal> or
+L<Qublog::Server::Controller::Content>.
 
 =head1 METHODS
 
-=cut
-
 =head2 index
+
+Forwards the user to L<Qublog::Server::Controller::Journal/index>.
 
 =cut
 
@@ -30,6 +32,13 @@ sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
     $c->forward('/journal/index');
 }
+
+=head2 default
+
+Tries the L<Qublog::Server::Controller::Content/index> handler. If that fails,
+shows a 404 error.
+
+=cut
 
 sub default :Path {
     my ($self, $c) = @_;
@@ -52,12 +61,25 @@ sub end : ActionClass('RenderView') {}
 
 =head1 AUTHOR
 
-Andrew Sterling Hanenkamp,,,
+Andrew Sterling Hanenkamp, C<< <hanenkamp@cpan.org> >>
 
 =head1 LICENSE
 
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
+Qublog Personal/Professional Journaling
+Copyright (C) 2009  Andrew Sterling Hanenkamp
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 =cut
 

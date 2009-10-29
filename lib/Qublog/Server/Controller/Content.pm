29d55be92aa97cc28e0df15449e1a06a1342e666
Sterling Hanenkamp <hanenkamp@cpan.org>
2009-10-28 21:02:43 -0500

    Fixing the POD for Qublog::Server::Controller::Content

--- lib/Qublog/Server/Controller/Content.pm
+++ lib/Qublog/Server/Controller/Content.pm
@@ -8,15 +8,20 @@ use File::Slurp qw( read_file );
 
 =head1 NAME
 
-Qublog::Server::Controller::Content - Catalyst Controller
+Qublog::Server::Controller::Content - Content manager for Qublog
 
 =head1 DESCRIPTION
 
-Catalyst Controller.
+This is the very simple content manager for Qublog. It serves static content
+found under the F<root/content> directory.
 
 =head1 METHODS
 
 =head2 index
+
+This takes the requested path and attempts to load the content of a C<.mkd> file
+matching it under F<root/content>. If such a file is found, it is slurped and
+passed off to the L<Qublog::Server::View::TD::Content/content/show> template.
 
 =cut
 
@@ -35,15 +40,27 @@ sub index :Path :Args(0) {
     }
 }
 
-
 =head1 AUTHOR
 
-Andrew Sterling Hanenkamp,,,
+Andrew Sterling Hanenkamp, C<< <hanenkamp@cpan.org> >>
 
 =head1 LICENSE
 
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
+Qublog Personal/Professional Journaling
+Copyright (C) 2009  Andrew Sterling Hanenkamp
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 =cut
 

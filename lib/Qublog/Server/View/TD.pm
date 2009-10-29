e26145f1ebdb35a7e0457c1482792a9f515420c7
Sterling Hanenkamp <hanenkamp@cpan.org>
2009-10-29 08:37:43 -0500

    Adding POD to Qubog::Server::View::TD

--- lib/Qublog/Server/View/TD.pm
+++ lib/Qublog/Server/View/TD.pm
@@ -8,3 +8,36 @@ use base 'Catalyst::View::Template::Declare';
 # Qublog::Server::View::TD::* namespace.  Be sure to "use Template::Declare::Tags"
 # in each one.
 
+=head1 NAME
+
+Qublog::Server::View::TD - Template::Declare views
+
+=head1 DESCRIPTION
+
+This is the Catalyst class for implementing the L<Template::Declare> infrastructure. See the classes under this namespace for the description of actual templates.
+
+=head1 AUTHOR
+
+Andrew Sterling Hanenkamp, C<< <hanenkamp@cpan.org> >>
+
+=head1 LICENSE
+
+Qublog Personal/Professional Journaling
+Copyright (C) 2009  Andrew Sterling Hanenkamp
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
+
+1;

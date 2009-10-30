8933abb20a14fb6fe215a0a59deabaf36b09b33b
Sterling Hanenkamp <hanenkamp@cpan.org>
2009-10-30 07:40:59 -0500

    Adding POD to Qublog::Schema::Result::JournalEntryTag

--- lib/Qublog/Schema/Result/JournalEntryTag.pm
+++ lib/Qublog/Schema/Result/JournalEntryTag.pm
@@ -3,6 +3,30 @@ use Moose;
 
 extends qw( Qublog::Schema::Result );
 
+=head1 NAME
+
+Qublog::Schema::Result::JournalEntryTag - link entries to tags
+
+=head1 DESCRIPTION
+
+Links journal entries to tags.
+
+=head1 SCHEMA
+
+=head2 id
+
+The autogenderated ID column.
+
+=head2 journal_entry
+
+The linked L<Qublog::Schema::Result::JournalEntry>.
+
+=head2 tag
+
+The linked L<Qublog::Schema::Result::Tag>.\\
+
+=cut
+
 __PACKAGE__->load_components(qw( Core ));
 __PACKAGE__->table('journal_entry_tags');
 __PACKAGE__->add_columns(
@@ -13,5 +37,29 @@ __PACKAGE__->add_columns(
 __PACKAGE__->set_primary_key('id');
 __PACKAGE__->belongs_to( journal_entry => 'Qublog::Schema::Result::JournalEntry' );
 __PACKAGE__->belongs_to( tag => 'Qublog::Schema::Result::Tag' );
+
+=head1 AUTHOR
+
+Andrew Sterling Hanenkamp, C<< <hanenkamp@cpan.org> >>
+
+=head1 LICENSE
+
+Qublog Personal/Professional Journaling
+Copyright (C) 2009  Andrew Sterling Hanenkamp
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
 
 1;

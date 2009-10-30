59f31197267896809084c64db32f33479629371a
Sterling Hanenkamp <hanenkamp@cpan.org>
2009-10-29 20:10:00 -0500

    Adding POD to Qublog::Schema::Result and Qublog::Schema::ResultSet

--- lib/Qublog/Schema/Result.pm
+++ lib/Qublog/Schema/Result.pm
@@ -3,8 +3,49 @@ use Moose;
 
 extends qw( DBIx::Class );
 
+=head1 NAME
+
+Qublog::Schema::Result - base class for Qublog result sources
+
+=head1 DESCRIPTIONo
+
+This is the base class for all the Qublgo result sources.
+
+=head1 METHODS
+
+=head2 _dumper_hook
+
+Used as the L<Data::Dumper> freezer method.
+
+=cut
+
 sub _dumper_hook { 
     $_[0] = bless { %{ $_[0] }, _source_handle => undef, }, ref($_[0]); 
 } 
+
+=head1 AUTHOR
+
+Andrew Sterling Hanenkamp, C<< <hanenkamp@cpan.org> >>
+
+=head1 LICENSE
+
+Qublog Personal/Professional Journaling
+Copyright (C) 2009  Andrew Sterling Hanenkamp
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
+
 
 1;
--- lib/Qublog/Schema/ResultSet.pm
+++ lib/Qublog/Schema/ResultSet.pm
@@ -3,4 +3,36 @@ use Moose;
 
 extends qw( DBIx::Class::ResultSet );
 
+=head1 NAME
+
+Qublog::Schema::ResultSet - base class for Qublog result sets
+
+=head1 DESCRIPTION
+
+This is a common base class for all custom result sets in Qublog.
+
+=head1 AUTHOR
+
+Andrew Sterling Hanenkamp, C<< <hanenkamp@cpan.org> >>
+
+=head1 LICENSE
+
+Qublog Personal/Professional Journaling
+Copyright (C) 2009  Andrew Sterling Hanenkamp
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
+
 1;

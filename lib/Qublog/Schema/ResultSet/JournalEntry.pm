6b5fce3b59a18d4bc32262984d8b29b86be40f1e
Sterling Hanenkamp <hanenkamp@cpan.org>
2009-11-02 21:22:54 -0600

    Add POD to Qublog::Schema::ResultSet::JournalEntry

--- lib/Qublog/Schema/ResultSet/JournalEntry.pm
+++ lib/Qublog/Schema/ResultSet/JournalEntry.pm
@@ -2,6 +2,22 @@ package Qublog::Schema::ResultSet::JournalEntry;
 use Moose;
 extends qw( Qublog::Schema::ResultSet );
 
+=head1 NAME
+
+Qublog::Search::ResultSet::JournalEntry - handy result set helpers for entries
+
+=head1 DESCRIPTION
+
+Helpers to the result set for journal entries.
+
+=head1 METHODS
+
+=head2 search_by_running
+
+Give it a boolean value. True indicates you want running entries. False indicates you want stopped entries.
+
+=cut
+
 sub search_by_running {
     my ($self, $running) = @_;
 
@@ -9,5 +25,29 @@ sub search_by_running {
         stop_time => { ($running ? '=' : '!='), undef },
     });
 }
+
+=head1 AUTHOR
+
+Andrew Sterling Hanenkamp, C<< <hanenkamp@cpan.org> >>
+
+=head1 LICENSE
+
+Qublog Personal/Professional Journaling
+Copyright (C) 2009  Andrew Sterling Hanenkamp
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
 
 1;

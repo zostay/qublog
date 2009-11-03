d483eceb7d5c7b91c72e9fa507d5889a07a8cc42
Sterling Hanenkamp <hanenkamp@cpan.org>
2009-11-02 21:09:40 -0600

    Adding POD for Qublog::Schema::ResultSet::JournalDay

--- lib/Qublog/Schema/ResultSet/JournalDay.pm
+++ lib/Qublog/Schema/ResultSet/JournalDay.pm
@@ -9,6 +9,24 @@ BEGIN {
 
 use Qublog::DateTime2;
 
+=head1 NAME
+
+Qublog::Schema::ResultSet::JournalDay - the results of journal day
+
+=head1 DESCRIPTION
+
+Handy result set methods.
+
+=haed1 METHODS
+
+=head2 for_date
+
+=head2 find_by_date
+
+Given a date returns, the journal day for the date. This will create it if it does not yet exist.
+
+=cut
+
 sub find_by_date {
     my ($self, $date) = @_;
     my $day = $date->clone->truncate( to => 'day' );
@@ -23,12 +41,44 @@ sub find_by_date {
     return $journal_day;
 }
 
+=head2 for_today
+
+=head2 find_today
+
+Given your notion of today, return the journal day for that date.
+
+=cut
+
 sub find_today {
     my ($self, $today) = @_;
     return $self->find_by_date( $today );
 }
 
-*for_date  = *find_date;
+*for_date  = *find_by_date;
 *for_today = *find_today;
+
+=head1 AUTHOR
+
+Andrew Sterling Hanenkamp, C<< <hanenkamp@cpan.org> >>
+
+=head1 LICENSE
+
+Qublog Personal/Professional Journaling
+Copyright (C) 2009  Andrew Sterling Hanenkamp
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
 
 1;

ec50aad56ee7012397f684bc36da48c55dd70398
Sterling Hanenkamp <hanenkamp@cpan.org>
2009-11-04 10:35:34 -0600

    Removing the Jifty implementation of Qublog

--- lib/Qublog/Model/JournalEntryCollection.pm
+++
@@ -1,9 +0,0 @@
-use strict;
-use warnings;
-
-package Qublog::Model::JournalEntryCollection;
-use base qw/ Qublog::TimedCollection /;
-
-sub record_class { 'Qublog::Model::JournalEntry' }
-
-1;
--- lib/Qublog/Model/JournalTimerCollection.pm
+++
@@ -1,9 +0,0 @@
-use strict;
-use warnings;
-
-package Qublog::Model::JournalTimerCollection;
-use base qw/ Qublog::TimedCollection /;
-
-sub record_class { 'Qublog::Model::JournalTimer' }
-
-1;
